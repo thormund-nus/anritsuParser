beced561a6ab19b4384856376f879906247177b3
Thormund <e0308991@u.nus.edu>
2023-03-20 22:09:04 +0800

    Adds logic for extracting Spectrum as numpy array
    Takes parsed_data attr and searches for spectrum-containing dictionary.
    This is then converted into a numpy 2D array for ease of use in pyplot.

--- src/anritsuparser.py
+++ src/anritsuparser.py
@@ -6,8 +6,8 @@ Test on MS2723B save files.
 TODO: Move into single-module distribution if src-layout is unnecessary.
 """
 
-import numpy
-import orjson
+import numpy as np
+from typing import Optional
 from pathlib import Path
 
 class SpectrumData(object):
@@ -20,8 +20,9 @@ class SpectrumData(object):
         true (functionality to be implemented); Otherwise just parses 
         single file as provided by path.
         """
+        
         if path and data:
-            raise Exception('Both data and path supplied. Choose either one or the other.')
+            raise Exception("Both data and path supplied. Choose either one or the other.")
         if data and parseAll:
             raise Exception()
 
@@ -40,21 +41,24 @@ class SpectrumData(object):
             
             if parseAll and path.is_dir():
                 if not path.is_dir():
-                    raise ValueError('Path provided is not a directory!')
+                    raise ValueError("Path provided is not a directory!")
                 else:
                     # To implement how to concat frequency sensibly
                     raise NotImplementedError()
 
             elif (not parseAll) and path.is_file():
                 self.parsed_data = self.parseFile(path)
-
+                try:
+                    self.spectrum = self.parseSpectrum(self.parsed_data)
+                except ValueError:
+                    print("No Spectrum Found")
             else:
-                raise ValueError()
+                raise ValueError("Path given conflicts with `parseAll`.")
 
         elif data:
             self.parsed_data = self.parseData(data)
+            self.spectrum = self.parseSpectrum(self.parsed_data)
 
-    
     def parseFile(self, path: Path):
         with open(path, mode = 'r') as f:  # ASCII vs Binary support?
             filedata = f.read()
@@ -86,7 +90,7 @@ class SpectrumData(object):
             elif level==0:
                 result[key] = value
             else:
-                # assumes that keys are provided in order of provision
+                # assumes that keys are provided in order of provision (python 3.7)
                 append(result[list(result.keys())[-1]],key,value, level-1)
 
         # intialisation
@@ -127,6 +131,68 @@ class SpectrumData(object):
         tmp_keys = [k for k, v in result.items() if v == {}]
         for k in tmp_keys:
             del result[k]
+            
+        # compress first level key value pairs into a metadata key
+        tmp_keys = [k for k, v in result.items() if isinstance(v, str)]
+        tmp_dict = {}
+        for k in tmp_keys:
+            tmp_dict[k] = result[k]
+            del result[k]
+        result['METADATA'] = tmp_dict
+
+        #reorder metadata as being the first key
+        desired_key_order = ['METADATA'] + list(result.keys())[:-1]
+        result = {k: result[k] for k in desired_key_order}
         return result
 
-        
+    def parseSpectrum(self, parsed_data: dict):
+        """Obtains numpy 2D array of (freq, power)
+
+        Does a key search to see if some nested dictionary contains keys
+        of only form "P_<int>". Afterwards, yield for spectrum.
+
+        Parameters
+        ----------
+        parsed_data : dict
+            Nested dictionary format of raw data, as obtained from 
+            parseData/parseFile method.
+        """
+        def search(nested_dict: dict) -> Optional[np.array]:
+            """Recursive depth-frist search on parsed_data dictionary for
+            spectrum data."""
+            keys = nested_dict.keys()
+            ascii_int = lambda c: 48 <= ord(c) <= 57
+            p_int = lambda x: x.startswith('P_') and all(map(ascii_int, x[2:]))
+            if all(map(p_int, keys)) and len(keys) > 0:
+                # dict key has been hit! time to return the numpy array
+                # fromiter only supports 1D arrays
+                # def get_y(dict_values: str):
+                #     return dict_values.split(' , ')[0]
+                # def get_x(dict_values: str):
+                #     return dict_values.split(' , ')[1].split(' ')[0] # removes units
+                get_y = lambda dict_values: dict_values.split(' , ')[0]
+                get_x = lambda dict_values: dict_values.split(' , ')[1].split(' ')[0]
+
+                # nested_dict['P_0'] = '<power>, <freq> MHz'
+                x = np.fromiter(map(get_x, nested_dict.values()), dtype=np.double)
+                y = np.fromiter(map(get_y, nested_dict.values()), dtype=np.double)
+                return np.vstack((x, y))
+
+            # key has not been hit, continue with DFS.
+            for k in keys.__reversed__():  # reversed for attempted optimization
+                if isinstance(nested_dict[k], dict):
+                    return search(nested_dict[k])
+            return None
+
+        if isinstance(result := search(parsed_data), np.ndarray):
+            return result
+        raise ValueError("Spectrum could not be found!")
+
+    def __repr__(self):
+        string = "Anritsu Spectrum Data\n" + '-'*21 
+        if 'path' in self.__dict__.keys():
+            string += "\nPath: " + str(self.path)
+        if 'parsed_data' in self.__dict__.keys():
+            string += '\n' + "\n".join([f"{k}: {v}" for k, v in self.parsed_data["METADATA"].items()])
+
+        return string
